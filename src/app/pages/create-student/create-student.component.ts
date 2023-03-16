0433b4fafbfc245c2bbbfbdc2f6de98ae961495e
VladanStar <vladancupric@gmail.com>
2023-03-16 13:47:22 +0100

    Add

--- src/app/pages/create-student/create-student.component.ts
+++ src/app/pages/create-student/create-student.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StudentService } from 'src/app/services/student.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-student',
   templateUrl: './create-student.component.html',
-  styleUrls: ['./create-student.component.css']
+  styleUrls: ['./create-student.component.css'],
 })
-export class CreateStudentComponent {
+export class CreateStudentComponent implements OnInit {
+   public studentForm: FormGroup;
 
+  // studentForm: any;
+  constructor(
+    public studentService: StudentService,
+    public formBuilder: FormBuilder,
+    public router: Router
+  ) {
+
+this.studentForm=formBuilder.group({
+name:[''],
+email:[""],
+student_course:[""],
+fees:['']
+})
+  }
+  ngOnInit(): void {}
+
+
+  onSubmit() {
+    console.log('Vladan');
+this.studentService.createStudent(this.studentForm.value)
+  }
 }
