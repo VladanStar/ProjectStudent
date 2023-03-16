fc10337ce137b8381a6f92059843b8fe91b04c72
VladanStar <vladancupric@gmail.com>
2023-03-16 12:54:45 +0100

    Add

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Student } from '../student';
 // import { AngularFireModule } from '@angular/fire/compat';
 
 @Injectable({
@@ -15,10 +16,46 @@ return this.angularFirestore
 .doc(id)
 .valueChanges()
 }
+
 getStudentList(){
 return this.angularFirestore
 .collection('student-collection')
 .snapshotChanges()
+}
+
+createStudent(student:Student){
+return new Promise<any>((resolve, reject) => {
+this.angularFirestore
+.collection('student-collection')
+.add(student)
+.then(response=>{console.log(response)}, error=>reject(error))
+})
+}
+
+deleteStudent(student:Student){
+return this.angularFirestore
+.collection('student-collection')
+.doc(student.id)
+.delete()
+}
+
+updateStudent(student:Student, id:any){
+return this.angularFirestore
+.collection('student-collection')
+.doc(id)
+.update(
+  {
+
+    name:student.name,
+    email:student.email,
+    student_course:student.student_course,
+    fees:student.fees,
+
+}
+
+)
+
+
 }
 
 }
