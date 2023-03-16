b62874e6da7c5e7edc35614c4848d292d4fd0a9a
VladanStar <vladancupric@gmail.com>
2023-03-16 13:06:01 +0100

    Ad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateStudentComponent } from './pages/create-student/create-student.component';
+import { ListStudentComponent } from './pages/list-student/list-student.component';
+import { EditStudentComponent } from './pages/edit-student/edit-student.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'',redirectTo:'/create', pathMatch:'full'},
+{path:'create', component:CreateStudentComponent},
+{path:'list-student',component:ListStudentComponent },
+{path:'update-student/:id', component:EditStudentComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
